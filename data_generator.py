5ac167f85e63d8d89dfb5d14cfb149fdcb3300b5
Mohamed Ashraf <117025882+Mohamed-Ashraf273@users.noreply.github.com>
2025-11-23 13:44:32 +0200

    Yarb (#6)
    * modify db
    
    * update db
    
    * high index
    
    * tree based
    
    * best approach
    
    * yaaarab
    
    * final scann yarb ysht8l
    
    * add datagen
    
    * improved scann
    
    * final scann
    
    * update data generator

--- data_generator.py
+++ data_generator.py
@@ -6,10 +6,15 @@ def get_all_rows(db_path) -> np.ndarray:
 
 vectors = get_all_rows('OpenSubtitles_en_20M_emb_64.dat')
 
-random_idx = np.random.randint(0, vectors.shape[0], size=15*10**6)
+random_idx = np.random.randint(0, vectors.shape[0], size=10**7)
+# sample
 sampled_vectors = vectors[random_idx]
+# shuffle
 np.random.shuffle(sampled_vectors)
+# add small noise
 sampled_vectors += np.random.normal(0, 0.01, sampled_vectors.shape)
-
-with open('OpenSubtitles_en_15M_emb_64.dat', 'wb') as f:
+# normalize
+sampled_vectors /= np.linalg.norm(sampled_vectors, axis=1, keepdims=True) + 1e-10
+# save
+with open('OpenSubtitles_en_10M_emb_64.dat', 'wb') as f:
     sampled_vectors.tofile(f)
